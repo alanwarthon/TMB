977f724eab83423ad5bfb3060c28155696ee12b8
alanwarthon <familiawarthon@gmail.com>
2025-10-03 20:45:52 -0700

    BMR Calculator v1 works fine!

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // HTML elements
 const calculateBtn = document.getElementById("btnCalculate");
+const txtAge = document.getElementById("txtAge");
+const txtWeight = document.getElementById("txtWeight");
+const txtHeight = document.getElementById("txtHeight");
+const results = document.getElementById("results");
 
 // Adding function to Button
 calculateBtn.addEventListener("click",calculateTBM);
@@ -7,7 +11,26 @@ calculateBtn.addEventListener("click",calculateTBM);
 // Function TBM
 function calculateTBM(){
 
-  console.log("button clicked");
   const sex_selected = document.querySelector('input[name="sex"]:checked').value;
-  console.log('Sex selected', sex_selected);
+
+  let age = txtAge.value;
+  let weight = txtWeight.value;
+  let height = txtHeight.value;
+
+  let BMR;
+
+  if (sex_selected === "man") {
+    BMR = (10 * weight) + (6.25 * height) - (5 * age) + 5;
+  } else if (sex_selected === "woman") {
+    BMR = (10 * weight) + (6.25 * height) - (5 * age) - 161;
+  }
+
+  console.log('Sex selected: ', sex_selected);
+  console.log('Age: ',age);
+  console.log('Weight: ',weight);
+  console.log('Height: ',height);
+  console.log('BMR: ',BMR);
+
+  final_msg = `BMR = ${BMR}`;
+  results.textContent=final_msg;
 }
